5f433fa04e5beeedf878ffb1e1063659927f81b1
Enter <350840291@qq.com>
2018-10-22 17:31:08 +0800

    stuct eval

--- samples/evaler/demo.go
+++ samples/evaler/demo.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/soniah/evaler"
+	"math/big"
 	"reflect"
+	"strings"
 )
 
 func main(){
@@ -12,10 +14,11 @@ func main(){
 	r,ok:=result.Float64()
 	fmt.Println("r:",ok,r)
 
-	user := User{5, 6.3, 3}
+	user := User{5, 6, 3}
 	data := Struct2Map(user)
-	fmt.Printf("struct2map得到的map内容为:%v", data)
-
+	data2 := Struct2Map2(&user)
+	fmt.Printf("struct2map得到的map内容为:%v \n", data)
+	fmt.Printf("struct2map2得到的map内容为:%v \n", data2)
 	ret, _ := evaler.EvalWithVariables("x + 1",  map[string]string{"x": "5"})
 	fmt.Println("EvalWithVariables:",ret.Num())
 
@@ -23,12 +26,17 @@ func main(){
 
 	fmt.Println("EvalWithVariables:",ret2.Num())
 
+	fmt.Println("计算前：",user.Username)
+	StructFormulaEqual(&user,"Username=Password + 1")
+
+	fmt.Println("计算后：",user.Username)
+
 }
 
 type User struct {
 	Id        int    `json:"id"`
-	Username    float64    `json:"username"`
-	Password    float64    `json:"password"`
+	Username    int    `json:"username"`
+	Password    int    `json:"password"`
 }
 
 func Struct2Map(obj interface{}) map[string]string {
@@ -43,4 +51,41 @@ func Struct2Map(obj interface{}) map[string]string {
 	return data
 }
 
+func Struct2Map2(obj interface{}) map[string]string {
+	var data = make(map[string]string)
+	s := reflect.ValueOf(obj).Elem()
+	typeOfT := s.Type()
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		data[typeOfT.Field(i).Name] = fmt.Sprint(f.Interface())
+	}
+	return data
+}
+// StructFormula 计算公式 "x+1"
+func StructFormula(obj interface{},formula string) (result *big.Rat, err error){
+	smap := Struct2Map2(obj)
+	return evaler.EvalWithVariables(formula,  smap)
+}
 
+// StructFormulaEqual 计算等式 "y=x+1"
+func StructFormulaEqual(obj interface{}, formula string) {
+	fs := strings.Split(formula, "=")
+	if len(fs)==2 {
+		if r,error := StructFormula(obj,fs[1]); error==nil{
+			s := reflect.ValueOf(obj).Elem()  // 反射获取测试对象对应的struct枚举类型
+			//strings.Replace(str, " ", "", -1)
+			field := s.FieldByName(fs[0])
+			switch t := field.Interface().(type) {
+			case int:
+				field.SetInt(r.Num().Int64())
+			case float64:
+				r,_:=r.Float64()
+				field.SetFloat(r)
+			default:
+				_ = t
+				panic("非数字")
+			}
+		}
+	}
+
+}
--- samples/inter_face/interface_main.4.go
+++ samples/inter_face/interface_main.4.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type I interface {
 	M1()
 }
@@ -10,5 +15,30 @@ func (T) M2() {}
 func main() {
 	var i I = T(10)
 	i.M1()
-	i.M2() // i.M2 undefined (type I has no field or method M2) 不能通过编译
+	//i.M2() // i.M2 undefined (type I has no field or method M2) 不能通过编译
+
+
+	type T struct {
+
+		A int
+
+		B string
+
+	}
+
+	t := T{23, "skidoo"}
+
+	s := reflect.ValueOf(&t).Elem()
+
+	typeOfT := s.Type()
+
+	for i := 0; i < s.NumField(); i++ {
+
+		f := s.Field(i)
+
+		fmt.Printf("%d: %s %s = %v\n", i,
+
+			typeOfT.Field(i).Name, f.Type(), f.Interface())
+
+	}
 }
